257e298b3e14badca038540f0766e97c772dafc7
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2024-01-28 18:37:59 +0530

    max length changed

--- text_generation_app/forms.py
+++ text_generation_app/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class TextGenerationForm(forms.Form):
     input_text = forms.CharField(label='Input Text', widget=forms.Textarea)
-    max_length = forms.IntegerField(label='Max Length', min_value=1, max_value=100)
+    max_length = forms.IntegerField(label='Max Length', min_value=1, max_value=1024)
