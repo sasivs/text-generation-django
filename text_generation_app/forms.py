3b5e49825042d8d5c94e6bf0113da83b88b8ebf7
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2024-02-26 19:56:58 +0530

    added submit button and text area size is updated

--- text_generation_app/forms.py
+++ text_generation_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class TextGenerationForm(forms.Form):
-    input_text = forms.CharField(label='Input Text', widget=forms.Textarea(attrs={'class':'text', 'id':'input-text','placeholder':'Input Text'}))
+    input_text = forms.CharField(label='Input Text', widget=forms.Textarea(attrs={'class':'text', 'id':'input-text','placeholder':'Input Text', "oninput":'this.style.height = "";this.style.height = this.scrollHeight + "px"'}))
     max_length = forms.IntegerField(label='Max Length', min_value=1, max_value=5000)
